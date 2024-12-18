1d0f7d4f6104fffd2526adbfc3320fe6d68c47a9
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-12-18 15:14:47 -0300

    docs: swagger at route

--- src/routers/notificationRouter.ts
+++ src/routers/notificationRouter.ts
@@ -5,6 +5,32 @@ const notificationRouter: FastifyPluginCallback = (app: FastifyInstance, options
     const notificationController = new NotificationController();
 
     app.post("/", {
+        schema: {
+            description: 'Envio de notificação para os usuários',
+            tags: ['Notification'],
+            body: {
+                type: 'object',
+                properties: {
+                    message: { type: 'string', description: 'Mensagem da notificação' },
+                },
+                required: ['message'],
+            },
+            response: {
+                201: {
+                    description: 'Mensagem enviada',
+                    type: 'object',
+                    properties: {
+                        message: { type: 'string' },
+                    },
+                },
+                500: {
+                    type: 'object',
+                    properties: {
+                        error: { type: 'string' },
+                    },
+                },
+            },
+        },
         handler: notificationController.create,
     });
 
